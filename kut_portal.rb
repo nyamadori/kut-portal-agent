03cc1025b7ba026601e6d747547a0add020acac8
nyamadori <nyamadorig@gmail.com>
2016-05-11 10:41:39 +0900

    ログイン失敗時の処理追加、クッキー復元処理分割

--- kut_portal.rb
+++ kut_portal.rb
@@ -1,6 +1,7 @@
 require 'mechanize'
 require 'openssl'
 require 'dotenv'
+require 'pp'
 
 class KUTPortal
   attr_reader :agent
@@ -11,6 +12,10 @@ class KUTPortal
     @agent = Mechanize.new { |a| a.user_agent_alias = 'Windows Mozilla' }
     # ポータルの証明書が原因でアクセスに失敗するため、証明書を確認しないようにする
     @agent.verify_mode = OpenSSL::SSL::VERIFY_NONE
+  end
+
+  def restore_session
+    return unless File.exist?('cookie.yml')
     @agent.cookie_jar.load('cookie.yml', session: true)
   end
 
@@ -24,15 +29,24 @@ class KUTPortal
     form['j_password'] = password
     form.click_button # submit
 
+    # ログイン失敗
+    return false if @agent.page.at('.form-element.form-error')
+
     # Mechanize は JavaScript をサポートしないため、ポータルサイトが警告を発するが
     # [Continue] ボタンをクリックして無視する
     @agent.page.forms[0].click_button
     @agent.cookie_jar.save('cookie.yml', session: true)
+
+    true
   end
 
   def logging_in?
     @agent.get INDEX_URL
     !!@agent.page.at('img[alt=ログアウト]')
+  end
+
+  def need_login?
+    !logging_in?
   end
 
   def ta_subjects
@@ -52,11 +66,23 @@ class KUTPortal
     subjects.shift
     subjects
   end
+
+  def record_ta_working_hours(time)
+
+  end
 end
 
 Dotenv.load
 
 portal = KUTPortal.new
 portal.start
-portal.login(ENV['USERNAME'], ENV['PASSWORD']) unless portal.logging_in?
-p portal.ta_subjects
+portal.restore_session
+
+if portal.need_login?
+  unless portal.login(ENV['USERNAME'], ENV['PASSWORD'])
+    puts 'ログイン失敗!'
+    exit 1
+  end
+end
+
+pp portal.ta_subjects
